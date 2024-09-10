d30afa7429ad5ec36ef19c84fe928aef741a20b2
Antoinette Belebele <anthobelebele@gmail.com>
2024-09-10 11:42:08 +0100

    add main section

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -3,7 +3,7 @@ import Tweet from "./tweet"
 
 function Main() {
   return (
-    <div className="w-96">
+    <div className="w-[47rem]">
         <div className="flex  justify-between  border-b border-gray-500">
             <p className="text-white">Home</p>
             <img className="hidden sm:block" src="/image_twitter/Vector.svg" alt="" />
@@ -30,7 +30,79 @@ function Main() {
           </div> 
         </div>
         <div>
-        <Tweet />
+        <Tweet
+          avatar="/image_twitter/image.png"
+          username="CNN"
+          handle="cnn"
+          time="7m"
+          content="President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a 'major breakthrough' that would serve to both strengthen the US steel industry and combat the global climate crisis."
+          numberLike="57"
+          numberCommit="144"
+          numberRetweet="184"
+          numberDownload=""
+          />
+
+        <Tweet
+          avatar="/image_twitter/image-1.png"
+          username="The New York Times"
+          handle="nytimes"
+          time="2h"
+          content="Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land."
+          image="/image_twitter/image-3.png"
+          numberLike="19"
+          numberCommit="48"
+          numberRetweet="482"
+          numberDownload=""
+          />
+
+        <Tweet
+          avatar="/image_twitter/image-logo.png"
+          username="Twitter"
+          handle="Twitter"
+          time="Oct 29"
+          content="BIG NEWS lol jk still Twitter."
+          numberLike="6.8K"
+          numberCommit="36.6K"
+          numberRetweet="267.1K"
+          numberDownload=""
+          />
+
+        <Tweet
+          avatar="/image_twitter/image-logo.png"
+          username="Twitter"
+          handle="Twitter"
+          time="Oct 29"
+          content="Hello literally everyone."
+          numberLike="118.7K"
+          numberCommit="785.4K"
+          numberRetweet="3.3M"
+          numberDownload=""
+          />
+        
+        <Tweet
+          avatar="/image_twitter/image-logo.png"
+          username="Twitter"
+          handle="Twitter"
+          time="Oct 4"
+          content="Hello literally everyone."
+          image="/image_twitter/image-3.png"
+          numberLike="118.7K"
+          numberCommit="785.4K"
+          numberRetweet="3.3M"
+          numberDownload=""
+          />
+
+        <Tweet
+          avatar="/image_twitter/image-logo.png"
+          username="Twitter"
+          handle="Twitter"
+          time="Oct 4"
+          content="Hello literally everyone."
+          numberLike="118.7K"
+          numberCommit="785.4K"
+          numberRetweet="3.3M"
+          numberDownload=""
+          />
         </div>
     </div>
   )
--- src/components/main/tweet.tsx
+++ src/components/main/tweet.tsx
@@ -1,14 +1,18 @@
 interface TweetProps {
-    avatar: String,
+    avatar: string,
     username: string,
     handle: string,
     time: string,
     content: string,
-    image: string
+    image?: string,
+    numberLike:string,
+    numberCommit:string,
+    numberRetweet: string,
+    numberDownload: string
 }
-const Tweet = ({ avatar, username, handle, time, content, image }: TweetProps) => {
+const Tweet = ({ avatar, username, handle, time, content, image, numberLike, numberCommit, numberRetweet, numberDownload }: TweetProps) => {
   return (
-    <div className="border-b border-gray-300 p-4 flex">
+    <div className="border-b border-gray-500 p-4 flex">
       <div className="mr-4">
         <img
           src={avatar}
@@ -19,35 +23,52 @@ const Tweet = ({ avatar, username, handle, time, content, image }: TweetProps) =
 
       <div className="flex-1">
         <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <span className="font-bold">{username}</span>
-            <span className="text-gray-500 ml-2">@{handle}</span>
-            <span className="text-gray-500 mx-2">·</span>
+          <div className="flex items-center gap-2">
+            <span className="font-bold text-white">{username}</span>
+            <img src="/image_twitter/badge.png" alt="" />
+            <span className="text-gray-500">@{handle}</span>
+            <span className="text-gray-500">·</span>
             <span className="text-gray-500">{time}</span>
           </div>
         </div>
 
-        <p className="mt-2 text-gray-900">{content}</p>
+        <p className="mt-2 text-white">{content}</p>
 
         {image && (
           <div className="mt-2">
-            <img src={image} alt="Tweet" className="rounded-lg" />
+            <img src={image} alt="Tweet" className="rounded-3xl w-full" />
           </div>
         )}
 
-        <div className="flex justify-around mt-4 text-gray-500">
-          <button className="hover:text-blue-400">
-            <i className="far fa-comment"></i> Comment
-          </button>
-          <button className="hover:text-green-400">
-            <i className="fas fa-retweet"></i> Retweet
-          </button>
-          <button className="hover:text-red-400">
-            <i className="far fa-heart"></i> Like
-          </button>
-          <button className="hover:text-blue-400">
-            <i className="far fa-share-square"></i> Share
-          </button>
+        <div className="flex gap-24 mt-4 text-gray-500">
+          <div className="flex gap-3 w-20">
+            <button>
+              <img src="/image_twitter/commentaire.png" alt="" />
+            </button>
+            <p className="text-gray-500">{numberCommit}</p>
+          </div>
+
+          <div className="flex gap-3 w-20">
+            <button>
+              <img src="/image_twitter/retweet.png" alt="" />
+            </button>
+            <p className="text-gray-500">{numberRetweet}</p>
+          </div>
+
+          <div className="flex gap-3 w-20">
+            <button>
+              <img src="/image_twitter/like.png" alt="" />
+            </button>
+            <p className="text-gray-500">{numberLike}</p>
+          </div>
+
+          <div className="flex gap-3 w-24">
+            <button>
+            <img src="/image_twitter/téléchargerIcon.png" alt="" />
+            </button>
+            <p className="text-gray-500">{numberDownload}</p>
+          </div>
+
         </div>
       </div>
     </div>
