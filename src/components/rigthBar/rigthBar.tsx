473dcbd42dc2a6981de352b2c7f7d4079196accc
Antoinette Belebele <anthobelebele@gmail.com>
2024-10-07 12:08:20 +0100

    add width in rigth bar

--- src/components/rigthBar/rigthBar.tsx
+++ src/components/rigthBar/rigthBar.tsx
@@ -29,7 +29,7 @@ function RigthBar() {
   ]
 
   return (
-    <div className="hidden lg:block border-l border-grayColor p-4 w-[28rem]">
+    <div className="hidden lg:block border-l border-grayColor p-4 w-[22rem]">
       <div className="sticky top-0 backdrop-blur-xl bg-black p-0">
         <div className="flex items-center h-10 rounded-full bg-grayColor p-4 gap-2 placeholder-graycolor2">
         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" className="text-graycolor2"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m21 21l-4.343-4.343m0 0A8 8 0 1 0 5.343 5.343a8 8 0 0 0 11.314 11.314"/></svg>
